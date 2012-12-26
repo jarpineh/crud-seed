81d2cf7f107afcad000ac643bd945e7a80a94890
jarpineh <jussi.arpalahti@gmail.com>
2012-12-26 20:53:50 +0200

    Back to hash bang app views

--- app/js/app.js
+++ app/js/app.js
@@ -3,9 +3,9 @@
 
 // Declare app level module which depends on filters, and services
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'ngSanitize']).
-  config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-  	$locationProvider.html5Mode(true);
-    $routeProvider.when('/app/view1', {templateUrl: 'partials/partial1.html', controller: MyCtrl1});
-    $routeProvider.when('/app/view2', {templateUrl: 'partials/partial2.html', controller: MyCtrl2});
-    $routeProvider.otherwise({redirectTo: '/app/view1'});
+  config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: MyCtrl1});
+    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: MyCtrl2});
+    $routeProvider.otherwise({redirectTo: '/view1'});
   }]);
+
