a8cb43f664af09e949916228dcdaf93751395ba0
jarpineh <jussi.arpalahti@gmail.com>
2012-12-27 00:54:01 +0200

    Example code for custom service
    Service is needed for saving form data to a REST endpoint. Need to read
    some docs first.

--- app/js/services.js
+++ app/js/services.js
@@ -7,3 +7,11 @@
 // In this case it is a simple value service.
 angular.module('myApp.services', []).
   value('version', '0.1');
+
+/*
+ angular.module('save', ['ngResource']).
+    factory('save_fform', function($resource){
+    	return $resource('/save', {}, {
+    query: {method:'POST', params:{data:'phones'}, isArray:true}
+  });
+});/*
