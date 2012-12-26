e5c7b6cffb616c3b13de0149abd1d9601986ede2
jarpineh <jussi.arpalahti@gmail.com>
2012-12-26 22:34:57 +0200

    First form displays with default JSON data and all.
    Example came from AngulaJS CookBook but it seems it's incompatible with the seed app.
    
    Controllers defined in a controller.js module need to use $inject to get $scope and others. Documentation of course documents the simple cases, while the seed app is divided in modules whose behaviour is different.

--- app/js/app.js
+++ app/js/app.js
@@ -6,6 +6,29 @@ angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: MyCtrl1});
     $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: MyCtrl2});
+    $routeProvider.when('/fform', {templateUrl: 'partials/first_form.html', controller: Ctrl});
     $routeProvider.otherwise({redirectTo: '/view1'});
   }]);
 
+/*
+angular.module('app').controller('personController', [
+  '$log', '$scope', '$location', 'personService', function($log, $scope, $location, personService) {
+    $scope.people = [];
+    $scope.insertPerson = function(person) {
+      return personService.save(person, function(person, getResponseHeaders) {
+        $scope.error = '';
+        $scope.person = {};
+        $scope.people.push(person);
+        return $location.path("/people/" + person.id);
+      }, function(error) {
+        if (error.status === 403) {
+          return $scope.error = error.data;
+        }
+      });
+    };
+    return personService.get(function(people) {
+      return $scope.people = people;
+    });
+  }
+]);
+*/
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -10,3 +10,21 @@ MyCtrl1.$inject = [];
 function MyCtrl2() {
 }
 MyCtrl2.$inject = [];
+
+function Ctrl($scope, $routeParams) {
+	alert($routeParams);
+	var master = {
+    name: 'John Smith',
+    address:{
+      line1: '123 Main St.',
+      city:'Anytown',
+      state:'AA',
+      zip:'12345'
+    },
+    contacts:[
+      {type:'phone', value:'1(234) 555-1212'}
+    ]
+  };
+  $scope.form = master;
+}
+Ctrl.$inject = ['$scope', '$routeParams']; //Where is this $inject documented and why it is needed?
